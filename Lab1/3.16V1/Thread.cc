9548488cce20e638aa259b17c9056fe4302f885d
Pixel-Lee <1253696851@qq.com>
2020-03-30 21:46:45 +0800

    Only reduce 50%

--- Lab1/3.16V1/Thread.cc
+++ Lab1/3.16V1/Thread.cc
@@ -5,14 +5,14 @@
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
+#include <list>
 #include "sudoku.h"
 
-// std::mutex mtx;
-// sem_t sem;
+char answer[1000][128];
 typedef struct Counter_t
 {
-    int value;
-    FILE* fp;
+    int value,size;
+    list<char *> sudoku;
     pthread_mutex_t lock;
 }Counter_t;
 Counter_t gloCoun,*p;
@@ -24,31 +24,43 @@ int64_t now()
   return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
-void init(Counter_t *c,char* filename)
+void init(Counter_t *c)
 {
     c->value=0;
-    c->fp = fopen(filename, "r");
     pthread_mutex_init(&c->lock,NULL);
 }
 
-bool readPuzzle(Counter_t *c,char *puzzle,int size)
+bool readPuzzle(Counter_t *c)
 {
+    int position=0;
     bool retBool;
     Solve solve;
     pthread_mutex_lock(&c->lock);
-    if(fgets(puzzle,128,c->fp)!=NULL)
+    if(c->sudoku.size()!=0)
     {
+	char puzzle[128];
+	position=c->value;
+	c->value++;
+	strcpy(puzzle,*(c->sudoku.begin()));
+	c->sudoku.pop_front();
 	pthread_mutex_unlock(&c->lock);
-	usleep(100);
+
 	solve.input(puzzle);
-	//cout<<puzzle<<' ';
 	if(solve.solve_sudoku_basic(0,puzzle))
 	{
-	    if (!solved(solve))  assert(0);
-	    cout<<"success"<<endl;
+	    if (!solved(solve)) assert(0);
+	    pthread_mutex_lock(&c->lock);
+	    strcpy(answer[position],puzzle);
+	    pthread_mutex_unlock(&c->lock);
+	    if(output) cout<<"success"<<endl;
 	}
 	else
-		cout<<"fail"<<endl;
+	{
+	    pthread_mutex_lock(&c->lock);
+	    strcpy(answer[position],"fail");
+	    pthread_mutex_unlock(&c->lock);
+	    if(output) cout<<"fail"<<endl;
+	}
 	return true;
     }
     else
@@ -61,18 +73,18 @@ bool readPuzzle(Counter_t *c,char *puzzle,int size)
 void* Deal(void* threadNum) 
 {
     int i=0;
-    char puzzle[128];
     bool roop=true;
     while(roop)
     {
-	cout<<"thread "<<threadNum<<endl;
-	roop=readPuzzle(p,puzzle,sizeof puzzle);
+	if(output) cout<<"thread "<<threadNum<<endl;
+	roop=readPuzzle(p);
     }
     return (void*)i;
 }
 
 int main()
 {
+    FILE* fp;
     init_neighbors();
     printf("input thread num: \n");
     int n;
@@ -83,11 +95,19 @@ int main()
     char filename[30];
     printf("input test file:");
     cin>>filename;
+    fp=fopen(filename, "r");
 
     int64_t start = now();
     p=&gloCoun;
-    init(p,filename);
-    
+    init(p);
+    while(true)
+    {	
+	char *puzzle=new char[128];
+	if(fgets(puzzle,128,fp)==NULL) break;
+	p->sudoku.push_back(puzzle);
+    }
+    p->size=p->sudoku.size();    
+
     for(int i=0;i<n;i++)
     {
 	if(pthread_create(&thread[i], NULL, Deal,(void*)i)!=0)
@@ -110,6 +130,17 @@ int main()
     for(int i=0;i<n;i++)
     {
 	printf("Thread%d return : %d\n",i,retval[i]);
+    }
+    
+    for(int i=0;i<p->size;i++)
+    {
+	cout<<answer[i]<<endl;
+    }
+
+    for(int i=0;p->sudoku.size()>0;i++)
+    {
+	*(p->sudoku.begin())=NULL;
+	p->sudoku.pop_front();
     }
     cout<<"Process time:"<<sec<<endl;
     return 0;
